f414e52d80189c811afe27f9a2deb3c2ffc45b86
N Zavala <noezh.25@gmail.com>
2024-06-13 03:13:38 -0600

    Add index title, add link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import { FaGithub } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <footer>
-      <span className="footer-text">Noe Zavala - GitHub @nxzav</span>
+      <span className="footer-text">
+        <a target="_blank" href="https://github.com/nxzav">
+          <FaGithub /> @nxzav
+        </a>
+      </span>
     </footer>
   );
 }
